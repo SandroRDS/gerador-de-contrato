1c70df2558297b2151b6e9fa8bf6850c709b085b
SandroRDS <sandror.desouza847@gmail.com>
2022-11-28 18:24:14 -0300

    Refatorando classes Endereco e Usuario

---
+++ php/classEndereco/Endereco.php
@@ -0,0 +1,11 @@
+<?php
+    class Endereco
+    {
+        public string $rua;
+        public string $numero;
+        public string $cep;
+        public string $bairro;
+        public string $uf;
+        public string $referencia;
+    }
+?>
---
+++ php/classUsuario/Usuario.php
@@ -0,0 +1,13 @@
+<?php
+    class Usuario
+    {
+        public string $nome;
+        public string $sobrenome;
+        public string $email;
+        public string $senha;
+        public object $endereco;
+        public string $celular;
+        public string $cpf;
+        public string $cnpj;
+    }
+?>
