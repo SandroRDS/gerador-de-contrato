7c5d4fae276be29758057bf76ca1e8af34c4e1af
SandroRDS <sandror.desouza847@gmail.com>
2022-11-30 20:35:36 -0300

    Código de validação de registro e inserção completo

--- php/validation/register-validation.php
+++ php/validation/register-validation.php
@@ -1,13 +1,83 @@
 <?php
-    include "php-classes/BDConection.php";
+    include "../classUsuario/Usuario.php";
+    include "../classEndereco/Endereco.php";
+    include "../classUserValidation/UserValidation.php";
+    include "../classBDConection/BDConection.php";
 
-    $conection = new BDConection();
-    $mysqli = $conection->criarConexao();
-
-    $result = $mysqli->query("SELECT * FROM usuario");
-    
-    while($linha = $result->fetch_array(MYSQLI_ASSOC))
+    if(isset($_POST["enviar"]))
     {
-        var_dump($linha);
+        $novo_endereco = new Endereco();
+        $novo_usuario = new Usuario();
+    
+        $novo_usuario->nome      = $_POST["usuario-nome"];
+        $novo_usuario->sobrenome = $_POST["usuario-sobrenome"];
+        $novo_usuario->email     = $_POST["usuario-email"];
+        $novo_usuario->senha     = $_POST["usuario-senha"];
+        $senha_repeat            = $_POST["usuario-senha-repeat"];
+        $novo_usuario->celular   = $_POST["usuario-celular"];
+        $novo_usuario->cpf       = $_POST["usuario-cpf"];
+        $novo_usuario->cnpj      = $_POST["usuario-cnpj"];
+        
+        $novo_endereco->rua        = $_POST["endereco-rua"];
+        $novo_endereco->numero     = $_POST["endereco-numero"];
+        $novo_endereco->cep        = $_POST["endereco-cep"];
+        $novo_endereco->bairro     = $_POST["endereco-bairro"];
+        $novo_endereco->uf         = $_POST["endereco-UF"];
+        $novo_endereco->referencia = $_POST["endereco-referencia"];
+    
+        $novo_usuario->endereco = $novo_endereco;
+
+        $validacao = new UserValidation($novo_usuario, $senha_repeat);
+        
+        if(empty($validacao->erros))
+        {
+            $conexaoBD = new BDConection();
+            $mysqli    = $conexaoBD->criarConexao();
+
+            $nome           = $validacao->usuario->nome;
+            $sobrenome      = $validacao->usuario->sobrenome;
+            $email          = $validacao->usuario->email;
+            $senha          = $validacao->usuario->senha;
+            $cpf            = $validacao->usuario->cpf;
+            $cnpj           = $validacao->usuario->cnpj;
+            $contato        = $validacao->usuario->celular;
+            $conta_aprovada = 0;
+
+            $logradouro = $validacao->usuario->endereco->rua;
+            $numero     = $validacao->usuario->endereco->numero;
+            $bairro     = $validacao->usuario->endereco->bairro;
+            $estado     = $validacao->usuario->endereco->uf;
+            $cep        = $validacao->usuario->endereco->cep;
+            $referencia = $validacao->usuario->endereco->referencia;
+            
+            $estrutura_usuario = "nome, sobrenome, email, senha, cpf, cnpj, contato, conta_aprovada";
+            $dados_usuario     = "'$nome', '$sobrenome', '$email', '$senha', '$cpf', '$cnpj', '$contato', $conta_aprovada";
+            
+            $verificacao_bd_usuario = $mysqli->query("SELECT * FROM usuario WHERE cpf = '$cpf' || cnpj = '$cnpj' || email = '$email' || contato = '$contato'");
+
+            if($verificacao_bd_usuario->num_rows < 1)
+            {
+                $insert_usuario = "INSERT INTO usuario ($estrutura_usuario) VALUES ($dados_usuario)";           
+                $mysqli->query($insert_usuario);
+                $idUsuario = $mysqli->insert_id;
+    
+                $estrutura_endereco = "idUsuario, logradouro, numero, bairro, estado, cep, referencia";
+                $dados_endereco = "$idUsuario, '$logradouro', $numero, '$bairro', '$estado', '$cep', '$referencia'";
+                $insert_endereco = "INSERT INTO endereco ($estrutura_endereco) VALUES ($dados_endereco)";
+                $mysqli->query($insert_endereco);
+            }
+            else
+            {
+                header("Location: ../../assets/templates/register.html");
+            }
+        }
+        else
+        {
+            http_response_code(400);
+        }
+    }
+    else
+    {
+        http_response_code(401);
     }
 ?>
