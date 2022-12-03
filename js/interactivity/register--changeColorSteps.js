9302cfeee65a1a3d308c590623d0983c7851a7e2
Rafael Silveira <Rafaellsilveira1357@gmail.com>
2022-12-03 13:44:36 -0300

    Criando validação de inputs da tela de cadastro

--- js/interactivity/register--changeColorSteps.js
+++ js/interactivity/register--changeColorSteps.js
@@ -4,22 +4,22 @@ const step2 = document.getElementById('step2');
 const separationStep1 = document.getElementById('separation--step1');
 const separationStep2 = document.getElementById('separation--step2');
 
-btnStep1Next.addEventListener("click", () => { 
-  step1.style.borderColor = 'blueviolet';
-  separationStep1.classList.add('ativo');
-});
+// btnStep1Next.addEventListener("click", () => { 
+//   step1.style.borderColor = 'blueviolet';
+//   separationStep1.classList.add('ativo');
+// });
 
-btnStep2Back.addEventListener("click", () => {
-  step1.style.borderColor = '#1d1d1d';
-  separationStep1.classList.remove('ativo');
-});
+// btnStep2Back.addEventListener("click", () => {
+//   step1.style.borderColor = '#1d1d1d';
+//   separationStep1.classList.remove('ativo');
+// });
 
-btnStep2Next.addEventListener("click", () => {
-  step2.style.borderColor = 'blueviolet';
-  separationStep2.classList.add('ativo');
-});
+// btnStep2Next.addEventListener("click", () => {
+//   step2.style.borderColor = 'blueviolet';
+//   separationStep2.classList.add('ativo');
+// });
 
-btnStep3Back.addEventListener("click", () => {
-  step2.style.borderColor = '#1d1d1d';
-  separationStep2.classList.remove('ativo');
-});
+// btnStep3Back.addEventListener("click", () => {
+//   step2.style.borderColor = '#1d1d1d';
+//   separationStep2.classList.remove('ativo');
+// });
--- js/interactivity/register--swiper.js
+++ js/interactivity/register--swiper.js
@@ -6,14 +6,14 @@ const swiper = new Swiper('.swiper', {
     spaceBetween: 15,
 });
 
-const btnStep1Next = document.querySelector('#step1next');
-btnStep1Next.onclick = () => swiper.slideNext();
+// const btnStep1Next = document.querySelector('#step1next');
+// btnStep1Next.onclick = () => nextStep1();
 
-const btnStep2Back = document.querySelector('#step2back');
-btnStep2Back.onclick = () => swiper.slidePrev();
+// const btnStep2Back = document.querySelector('#step2back');
+// btnStep2Back.onclick = () => backStep1();
 
-const btnStep2Next = document.querySelector('#step2Next');
-btnStep2Next.onclick = () => swiper.slideNext();
+// const btnStep2Next = document.querySelector('#step2Next');
+// btnStep2Next.onclick = () => swiper.slideNext();
 
-const btnStep3Back = document.querySelector('#step3back');
-btnStep3Back.onclick = () => swiper.slidePrev();
+// const btnStep3Back = document.querySelector('#step3back');
+// btnStep3Back.onclick = () => swiper.slidePrev();
